1b640548f2168a6386e96a04e48250caa27efae9
Artur <artmucha@bitbucket.org>
2021-05-11 20:23:01 +0200

    add simple manu

---
+++ constans/menu.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    text: 'Aktualności',
+    link: 'aktualnosci'
+  },
+  {
+    text: 'Najlepsze gry',
+    link: 'najlepsze-gry'
+  },
+  {
+    text: 'Rankingi',
+    link: 'rankingi',
+    sub: [
+      {
+        text: 'PC',
+        link: 'pc'
+      },
+      {
+        text: 'PlayStation 5',
+        link: 'playstation-5'
+      },
+      {
+        text: 'Xbox Series X',
+        link: 'xbox-series-x'
+      },
+      {
+        text: 'PlayStation 4',
+        link: 'playstation-4'
+      },
+      {
+        text: 'Xbox One',
+        link: 'xbox-one'
+      },
+      {
+        text: 'Switch',
+        link: 'nintendo-switch'
+      },
+      {
+        text: '3DS',
+        link: 'nintendo-3ds'
+      },
+      {
+        text: 'iOS',
+        link: 'ios'
+      },
+      {
+        text: 'Android',
+        link: 'android'
+      },
+    ],
+  },
+];
