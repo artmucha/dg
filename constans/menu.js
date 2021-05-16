5e2a594319e8c6b27ffe7e28f9fa989857f52bc0
Artur <artmucha@bitbucket.org>
2021-05-16 20:09:45 +0200

    finished basic layout

--- constans/menu.js
+++ constans/menu.js
@@ -8,45 +8,39 @@ export default [
     link: 'najlepsze-gry'
   },
   {
-    text: 'Rankingi',
-    link: 'rankingi',
-    sub: [
-      {
-        text: 'PC',
-        link: 'pc'
-      },
-      {
-        text: 'PlayStation 5',
-        link: 'playstation-5'
-      },
-      {
-        text: 'Xbox Series X',
-        link: 'xbox-series-x'
-      },
-      {
-        text: 'PlayStation 4',
-        link: 'playstation-4'
-      },
-      {
-        text: 'Xbox One',
-        link: 'xbox-one'
-      },
-      {
-        text: 'Switch',
-        link: 'nintendo-switch'
-      },
-      {
-        text: '3DS',
-        link: 'nintendo-3ds'
-      },
-      {
-        text: 'iOS',
-        link: 'ios'
-      },
-      {
-        text: 'Android',
-        link: 'android'
-      },
-    ],
+    text: 'PC',
+    link: 'pc'
+  },
+  {
+    text: 'PlayStation 5',
+    link: 'playstation-5'
+  },
+  {
+    text: 'Xbox Series X',
+    link: 'xbox-series-x'
+  },
+  {
+    text: 'PlayStation 4',
+    link: 'playstation-4'
+  },
+  {
+    text: 'Xbox One',
+    link: 'xbox-one'
+  },
+  {
+    text: 'Switch',
+    link: 'nintendo-switch'
+  },
+  {
+    text: '3DS',
+    link: 'nintendo-3ds'
+  },
+  {
+    text: 'iOS',
+    link: 'ios'
+  },
+  {
+    text: 'Android',
+    link: 'android'
   },
 ];
--- styles/theme.js
+++ styles/theme.js
@@ -1,8 +1,11 @@
 export const theme = {
   background: '#121212',
   surface: '#272727',
-  white: '#ffffff',
+  textprimary: '#ffffff',
+  textsecondary: 'rgba(255, 255, 255, 0.7)',
+  textthirtary: 'rgba(255, 255, 255, 0.4)',
   primary: '#5E35B1',
+  action: 'rgba(255, 255, 255, 0.08)',
 
   light: 300,
   regular: 400,
