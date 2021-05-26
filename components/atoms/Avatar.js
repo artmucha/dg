fec107bf026210f96cba7ba40bdc14282b2e4e68
Artur <artmucha@bitbucket.org>
2021-05-26 18:47:52 +0200

    add Avatar compoenent

---
+++ components/atoms/Avatar.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Avatar = styled.img`
+  display: block;
+  width: 100%;
+  max-width: 128px;
+  height: auto;
+  object-fit: cover;
+  border: 0;
+  border-radius: 50%;
+`;
+
+export default Avatar;
+ 
