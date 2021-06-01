7dc0377233190b5e885b502b8d36270ff3fc4293
Artur <artmucha@bitbucket.org>
2021-06-01 18:09:26 +0200

    add global Typogrphy component

---
+++ components/atoms/Typography.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components';
+
+const Typography = styled.div`
+  display: block;
+  font-weight: ${({ theme }) => theme.regular};
+  color: ${({ theme, color }) => (color ? theme[color] : theme.textprimary)};
+  font-size: ${({ theme, fontSize }) => (fontSize ? theme.fontSize[fontSize] : '16px')};
+
+  ${({ uppercase }) =>
+    uppercase &&
+    css`
+      text-transform: uppercase;
+  `}
+
+  ${({ bold }) =>
+    bold &&
+    css`
+      font-weight: ${({ theme }) => theme.bold};
+  `}
+
+  ${({ space }) => 
+    space &&
+    css`
+      margin: 20px 0;
+    `}
+
+`;
+
+export default Typography;
